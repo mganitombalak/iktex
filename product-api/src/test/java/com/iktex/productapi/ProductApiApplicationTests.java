e9e7e470367e98a2d2c739e5009d6a9371cce921
mganitombalak <mgani.tombalak@yahoo.com>
2020-01-09 10:11:39 +0300

    Product API

--- product-api/src/test/java/com/iktex/productapi/ProductApiApplicationTests.java
+++ product-api/src/test/java/com/iktex/productapi/ProductApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.iktex.categoryapi;
+package com.iktex.productapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CategoryApiApplicationTests {
+class ProductApiApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
